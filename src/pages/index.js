915342d0f08410ef936a300d6f6aa4a596ccae8a
Ilya Zverkov <izverk78@yandex.ru>
2022-08-19 15:53:28 +0300

    refactor: доработаны стили для совместимости с вордпрессом + добавлена возможность простановки маркера по координатам в map.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,69 +13,87 @@ import MapPopup from '../utils/MapPopup.js';
 import { mapCenterCoords } from '../utils/constants.js';
 import Tooltip from '../utils/Tooltip';
 
-const options = {
-	apiKey: 'AIzaSyAozRsHSFgLEZkH-Du-a2r4K1CsXPLjj2o',
-	sheetId: '1j9Ln-t7BoitA7fzeFp20tVq68h0KYO6x3PHjic4jqMA',
-	sheetName: 'Лист1',
-}
+document.addEventListener('DOMContentLoaded', startCalendarScripts);
 
-// Секция с ивентами
-const eventsElement = document.querySelector('.events');
+function startCalendarScripts() {
+	const options = {
+		apiKey: 'AIzaSyAozRsHSFgLEZkH-Du-a2r4K1CsXPLjj2o',
+		sheetId: '1j9Ln-t7BoitA7fzeFp20tVq68h0KYO6x3PHjic4jqMA',
+		sheetName: 'Лист1',
+	};
 
-// Создаем экземпляр попапа карты
-const mapPopup = new MapPopup('.map-popup', () => initMap(mapCenterCoords, eventPopup.currentEvent.address));
+	// Секция с ивентами
+	const eventsElement = document.querySelector('.events');
 
-// Создаем экземпляр попапа события
-const eventPopup = new EventPopup('.event-popup', { onClose: () => eventsElement.classList.remove('hidden'), onMapOpen: () => mapPopup.open() });
+	// // Создаем экземпляр попапа карты
+	// const mapPopup = new MapPopup('.map-popup', () => initMap(mapCenterCoords, eventPopup.currentEvent.address));
+	// Создаем экземпляр попапа карты
+	const mapPopup = new MapPopup('.map-popup', mapCenterCoords, initMap);
 
-// Создаем экземпляр тултипа
-const tooltip = new Tooltip('.tooltip');
+	// // Создаем экземпляр попапа события
+	// const eventPopup = new EventPopup('.event-popup', {
+	// 	onClose: () => eventsElement.classList.remove('hidden'),
+	// 	onMapOpen: () => mapPopup.open(),
+	// });
+	// Создаем экземпляр попапа события
+	const eventPopup = new EventPopup('.event-popup', {
+		onClose: () => eventsElement.classList.remove('hidden'),
+		onMapOpen: () => mapPopup.open(),
+	});
 
-const eventTemplateSelector = '.event-template';
+	// Создаем экземпляр тултипа
+	const tooltip = new Tooltip('.tooltip');
 
-function handleEventClick(event) {
-	if (eventsElement.clientWidth > 425) return;
-	eventsElement.classList.add('hidden');
-	eventPopup.open(event);
-}
+	const eventTemplateSelector = '.event-template';
 
-function showTooltip(type, evt) {
-	tooltip.show(iconTexts[type], evt);
-}
+	function handleEventClick(event) {
+		if (eventsElement.clientWidth > 425) return;
+		eventsElement.classList.add('hidden');
+		mapPopup.getEvent(event);
+		eventPopup.open(event);
+	}
 
-function renderEvents(events, period = 'month') {
-	eventPopup.close();
-	const titleStyle = period === 'month' && eventsElement.clientWidth > 425 ? 'full' : 'brief';
-	eventsElement.innerHTML = '';
-	let currentDate = '';
-	let wrapperElement = null;
-	events.forEach(event => {
-		if (currentDate !== event.date) {
-			const eventsDayElement = document.querySelector('.events-template').content.cloneNode(true).querySelector('.events');
-			const titleElement = eventsDayElement.querySelector('.events__title');
-			wrapperElement = eventsDayElement.querySelector('.events__container');
-			console.log(period, titleStyle);
-			titleElement.textContent = titleStyle === 'brief' ? makeSideDate(event.date) : makeTitleDate(event.date);
-			if (titleStyle === 'brief') {
-				eventsDayElement.classList.add('events_title_sideway');
-				titleElement.classList.add('events__title_brief');
+	function showTooltip(type, evt) {
+		tooltip.show(iconTexts[type], evt);
+	}
+
+	function renderEvents(events, period = 'month') {
+		eventPopup.close();
+		const titleStyle = period === 'month' && eventsElement.clientWidth > 425 ? 'full' : 'brief';
+		eventsElement.innerHTML = '';
+		let currentDate = '';
+		let wrapperElement = null;
+		events.forEach((event) => {
+			if (currentDate !== event.date) {
+				const eventsDayElement = document
+					.querySelector('.events-template')
+					.content.cloneNode(true)
+					.querySelector('.events');
+				const titleElement = eventsDayElement.querySelector('.events__title');
+				wrapperElement = eventsDayElement.querySelector('.events__container');
+				console.log(period, titleStyle);
+				titleElement.textContent = titleStyle === 'brief' ? makeSideDate(event.date) : makeTitleDate(event.date);
+				if (titleStyle === 'brief') {
+					eventsDayElement.classList.add('events_title_sideway');
+					titleElement.classList.add('events__title_brief');
+				}
+				if (isToday(event.date)) titleElement.classList.add('events__title_selected');
+				eventsElement.append(eventsDayElement);
+				currentDate = event.date;
 			}
-			if (isToday(event.date)) titleElement.classList.add('events__title_selected');
-			eventsElement.append(eventsDayElement);
-			currentDate = event.date;
-		}
-		wrapperElement.append(
-			new Event({
-				templateSelector: eventTemplateSelector,
-				event,
-				onClick: () => handleEventClick(event),
-				onMouseOver: showTooltip,
-				onMouseOut: tooltip.hide,
-			}))
+			wrapperElement.append(
+				new Event({
+					templateSelector: eventTemplateSelector,
+					event,
+					onClick: () => handleEventClick(event),
+					onMouseOver: showTooltip,
+					onMouseOut: tooltip.hide,
+				})
+			);
+		});
+	}
+
+	gsheets(options).then((data) => {
+		initCalendar(calendarContainerSelector, renderEvents, data);
 	});
 }
-
-gsheets(options).then(data => {
-	initCalendar(calendarContainerSelector, renderEvents, data);
-});
-
--- src/utils/MapPopup.js
+++ src/utils/MapPopup.js
@@ -1,13 +1,19 @@
 export default class MapPopup {
-	constructor(selector, addPlaceMarker) {
+	constructor(selector, mapCenterCoords, initMap) {
 		this._popupOpenedClass = 'map-popup_opened';
 		this._element = document.querySelector(selector);
 		this._closeButton = this._element.querySelector('.map-popup__close-button');
-		this._addPlaceMarker = addPlaceMarker;
+		this._mapCenterCoords = mapCenterCoords;
+		this._initMap = initMap;
+		this._event = {};
+	}
+
+	getEvent(event) {
+		this._event = event;
 	}
 
 	open() {
-		this._addPlaceMarker();
+		this._initMap(this._mapCenterCoords, this._event);
 		this._setEventListeners();
 		this._element.classList.add(this._popupOpenedClass);
 	}
