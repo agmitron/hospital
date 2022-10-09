f5f0b6c54592571fbf191b88d0e105dc228ad01d
Ilya Zverkov <izverk@yandex78.ru>
2022-10-09 18:06:02 +0300

    feat: добавлена функциональность кнопки share

--- src/utils/EventPopup.js
+++ src/utils/EventPopup.js
@@ -4,6 +4,7 @@ export default class EventPopup {
 		this._popupOpenedClass = 'calendar-event-popup_opened';
 		this._element = document.querySelector(selector);
 		this._closeButton = this._element.querySelector('.calendar-icon_type_close');
+		this._shareButton = this._element.querySelector('.calendar-icon_type_share');
 		this._howToReachButton = this._element.querySelector('.calendar-event-popup__button_red');
 		this._title = this._element.querySelector('.calendar-event-popup__title');
 		this._address = this._element.querySelector('.calendar-event-popup__text_address');
@@ -27,7 +28,7 @@ export default class EventPopup {
         this._date.textContent = this._convertDate(date);
         this._hours.textContent = hours;
         this._activities.innerHTML = '';
-		this.currentEvent = { title, address, activities, metro, hours, date };
+				this.currentEvent = { title, address, activities, metro, hours, date };
         activities.split(' ').filter(x => x).forEach(icon => this._activities.append(this._createActivity(icon, iconTexts[icon])));
         this._setEventListeners();
     }
@@ -67,16 +68,30 @@ export default class EventPopup {
 		if (evt.key === 'Escape') this.close();
 	};
 
+	_handleShareButton = async () => {
+		try {
+      await navigator.share({
+        text: `${this.currentEvent.title}, ${this.currentEvent.date}, ${this.currentEvent.hours}, ${this.currentEvent.address}, ${this.currentEvent.metro}, ${this.currentEvent.activities}`,
+        title: 'Мобильная клиника',
+        url: document.location.href,
+      });
+    } catch (err) {
+      alert(`Ошибка: ${err}`);
+    }
+  };
+
 	_setEventListeners() {
 		this._closeButton.addEventListener('click', this._handleCloseButton);
 		document.addEventListener('keydown', this._handleEscClose);
 		this._howToReachButton.addEventListener('click', this._openMapPopup);
+		this._shareButton.addEventListener('click', this._handleShareButton);
 	}
 
 	_removeEventListeners() {
 		document.removeEventListener('keydown', this._handleEscClose);
 		this._closeButton.removeEventListener('click', this._handleCloseButton);
 		this._howToReachButton.removeEventListener('click', this._openMapPopup);
+		this._shareButton.removeEventListener('click', this._handleShareButton);
 	}
 }
 
